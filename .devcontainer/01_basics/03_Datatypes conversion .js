30aad4b9de21b788bfb9a3aa40b83f1fe51135a9
shahbaz2201 <145041402+shahbaz2201@users.noreply.github.com>
2023-09-16 09:50:56 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_Datatypes conversion .js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log (typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" => 33
+// "3abc" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = "shahbaz"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false
+// "shahbaz" => true
+
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
