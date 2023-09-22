a7471b965f10c6ecc411dc7380627c7bb5f73ec6
shahbaz2201 <145041402+shahbaz2201@users.noreply.github.com>
2023-09-22 22:57:12 +0530

    02_Array_part2.js

--- .devcontainer/02_basics/02_Array_part2.js
+++ .devcontainer/02_basics/02_Array_part2.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Shahbaz"))
+console.log(Array.from("Shahbaz"))
+console.log(Array.from({name: "Shahbaz"})) // interesting
 
 let score1 = 100
 let score2 = 200
